21c49bac83ec6ccbfb212f9aa00dda75da87a28f
Vivek kumar <vivekkumar031096@gmail.com>
2024-10-04 20:20:38 +0530

    userservice_security_working

--- src/main/java/com/userservice2/userservice2/services/AuthService.java
+++ src/main/java/com/userservice2/userservice2/services/AuthService.java
@@ -14,6 +14,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.MacAlgorithm;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,7 @@ import java.util.*;
 
 @Service
 public class AuthService {
+    @Autowired
     private UserRepository userRepository;
     private SessionRepository sessionRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
